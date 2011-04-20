42a77f5e7e5d010efa386d34a6805dcd5bf811ed
Guntis Ozols <guntiso@uniso.lv>
2011-04-20 08:28:00 +0300

    query test webapp - pars 2 to 5 binding fixed

--- service/src/main/scala/code/snippet/AjaxQuery.scala
+++ service/src/main/scala/code/snippet/AjaxQuery.scala
@@ -19,10 +19,10 @@ object AjaxQuery {
     def process(): JsCmd = {
       S.notice("Query: " + queryString)
       S.notice("Par1: " + par1)
-      S.notice("Par2: " + par1)
-      S.notice("Par3: " + par1)
-      S.notice("Par4: " + par1)
-      S.notice("Par5: " + par1)
+      S.notice("Par2: " + par2)
+      S.notice("Par3: " + par3)
+      S.notice("Par4: " + par4)
+      S.notice("Par5: " + par5)
       try {
         resultString = //
           QueryServer.json(queryString, List(par1, par2, par3, par4, par5))
@@ -35,6 +35,10 @@ object AjaxQuery {
     "#result *" #> resultString &
       "name=query" #> SHtml.textarea(queryString, queryString = _) &
       "name=par1" #> SHtml.text(par1, par1 = _) &
+      "name=par2" #> SHtml.text(par2, par2 = _) &
+      "name=par3" #> SHtml.text(par3, par3 = _) &
+      "name=par4" #> SHtml.text(par4, par4 = _) &
+      "name=par5" #> SHtml.text(par5, par5 = _) &
       "#hideme" #> SHtml.hidden(process)
   }
 }
