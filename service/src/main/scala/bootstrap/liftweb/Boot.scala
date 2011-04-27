33cc3bc59bc69b3e48478c70b0ba9c8194270c5a
Guntis Ozols <guntiso@uniso.lv>
2011-04-28 02:18:46 +0300

    serve json query result for http get

--- service/src/main/scala/bootstrap/liftweb/Boot.scala
+++ service/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,16 +1,12 @@
 package bootstrap.liftweb
 
+import code.QueryServer
+import net.liftweb.common._
+import net.liftweb.http._
+import net.liftweb.mapper._
+import net.liftweb.sitemap._
+import net.liftweb.util._
 import net.liftweb._
-import util._
-import Helpers._
-
-import common._
-import http._
-import sitemap._
-import Loc._
-import mapper._
-
-
 
 /**
  * A class that's instantiated early and run.  It allows the application
@@ -19,28 +15,32 @@ import mapper._
 class Boot {
   def boot {
     if (!DB.jndiJdbcConnAvailable_?) {
-      val vendor = 
-	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
-			     Props.get("db.url") openOr 
-			     "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
-			     Props.get("db.user"), Props.get("db.password"))
+      val vendor =
+        new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+          Props.get("db.url") openOr
+            "jdbc:h2:lift_proto.db;AUTO_SERVER=TRUE",
+          Props.get("db.user"), Props.get("db.password"))
 
       LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
 
       DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
     }
 
+    // stateless handlers
+    LiftRules.statelessDispatchTable.append(QueryServer)
+
     // where to search snippet
     LiftRules.addToPackages("code")
 
     // Build SiteMap
     def sitemap = SiteMap(
-      Menu.i("Home") / "index")
+      Menu.i("Home") / "index",
+      Menu.i("GET") / "get")
 
     //Show the spinny image when an Ajax call starts
     LiftRules.ajaxStart =
       Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
-    
+
     // Make the spinny image go away when it ends
     LiftRules.ajaxEnd =
       Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
--- service/src/main/scala/code/QueryServer.scala
+++ service/src/main/scala/code/QueryServer.scala
@@ -1,21 +1,50 @@
 package code
 
+import java.io.{ OutputStreamWriter, CharArrayWriter, Writer, OutputStream }
+import net.liftweb.http.rest._
+import net.liftweb.http._
+import net.liftweb._
 import uniso.query.result._
 import uniso.query._
 
-object QueryServer {
-  def json(expr: String, pars: List[String]): String = //
-    json("com.sap.dbtech.jdbc.DriverSapDB", "burvis", "burfull2", expr, pars)
+object QueryServer extends RestHelper {
+
+  serve {
+    // FIXME served with GET - ensure query is readonly!!!
+    case JsonGet("query" :: Nil, _) =>
+      for {
+        query <- S.param("query") ?~ "query is missing" ~> 400
+      } yield {
+        OutputStreamResponse( //
+          (os: OutputStream) => json(query, S.params("p"), os),
+          List("Content-Type" -> "application/json"));
+      }
+  }
+
+  def json(expr: String, pars: List[String]): String = {
+    val writer = new CharArrayWriter
+    json(expr, pars, writer)
+    writer.toString
+  }
+
+  def json(expr: String, pars: List[String], os: OutputStream) {
+    val writer = new OutputStreamWriter(os, "UTF-8")
+    json(expr, pars, writer)
+    writer.flush
+  }
+
+  def json(expr: String, pars: List[String], writer: Writer) { //
+    json("com.sap.dbtech.jdbc.DriverSapDB", "burvis", "burfull2", expr, pars, writer)
+  }
 
   def json(jdbcDriverClass: String, user: String, password: String, //
-    expr: String, pars: List[String]): String = {
+    expr: String, pars: List[String], writer: Writer) {
     Class.forName(jdbcDriverClass)
     val md = metadata.JDBCMetaData(user, password)
     val conn = Conn()()
     val env = new Env(Map(), md, conn)
     Env.metaData(metadata.JDBCMetaData(user, password))
-    val writer = new java.io.CharArrayWriter
     Jsonizer.jsonize(Query(expr, pars)(conn), writer)
-    writer.toString
+    conn.close
   }
 }
