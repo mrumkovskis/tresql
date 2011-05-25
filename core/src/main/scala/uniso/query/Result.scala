2bedfffbbf0205c6d1c9bc6eb1ac8ca3504f687f
Martins Rumkovskis <martins@uniso.lv>
2011-05-25 16:46:36 +0300

    jdbc result set specific method invocation added

--- core/src/main/scala/uniso/query/Result.scala
+++ core/src/main/scala/uniso/query/Result.scala
@@ -1,9 +1,11 @@
 package uniso.query
 
 import java.sql.ResultSet
+import java.sql.ResultSetMetaData
 
 class Result private[query] (rs: ResultSet, cols: Vector[Column], reusableStatement: Boolean)
   extends Iterator[RowLike] with RowLike {
+  private[this] val md = rs.getMetaData
   private[this] val colMap = cols.filter(_.name != null).map(c => (c.name, c)).toMap
   private[this] val row = new Array[Any](cols.length)
   private[this] var hn = true; private[this] var flag = true
@@ -19,16 +21,16 @@ class Result private[query] (rs: ResultSet, cols: Vector[Column], reusableStatem
   }
   def next = { flag = true; this }
   def apply(columnIndex: Int) = {
-    if (cols(columnIndex).idx != -1) rs.getObject(cols(columnIndex).idx)
+    if (cols(columnIndex).idx != -1) asAny(cols(columnIndex).idx)
     else row(columnIndex)
   }
   def apply(columnLabel: String) = {
     try {
       colMap(columnLabel) match {
-        case Column(i, _, null) => rs.getObject(i)
+        case Column(i, _, null) => asAny(i)
         case Column(i, _, e) => row(i)
       }
-    } catch { case _: NoSuchElementException => rs.getObject(columnLabel) }
+    } catch { case _: NoSuchElementException => asAny(rs.findColumn(columnLabel)) }
   }
   def columnCount = cols.length
   def column(idx: Int) = cols(idx)
@@ -57,18 +59,32 @@ class Result private[query] (rs: ResultSet, cols: Vector[Column], reusableStatem
   /** needs to be overriden since super class implementation calls hasNext method */
   override def toString = getClass.toString + ":" + (cols.mkString(","))
 
+  private def asAny(pos: Int): Any = {
+    import java.sql.Types._
+    md.getColumnType(pos) match {
+      case ARRAY | BINARY | BLOB | DATALINK | DISTINCT | JAVA_OBJECT | LONGVARBINARY | NULL |
+        OTHER | REF | STRUCT | VARBINARY => rs.getObject(pos)
+      case DECIMAL | NUMERIC => rs.getBigDecimal(pos)
+      case BIGINT | INTEGER | /* DECIMAL | NUMERIC | */ SMALLINT | TINYINT => rs.getLong(pos)
+      case BIT | BOOLEAN => rs.getBoolean(pos)
+      case VARCHAR | CHAR | CLOB | LONGVARCHAR => rs.getString(pos)
+      case DATE => rs.getDate(pos)
+      case TIME | TIMESTAMP => rs.getTimestamp(pos)
+      case DOUBLE | FLOAT | REAL => rs.getDouble(pos)
+    }
+  }
+  
   case class Row(row: Seq[Any]) extends RowLike {
     def apply(idx: Int) = row(idx)
     def content = row
     def columnCount = row.length
     def column(idx: Int) = Result.this.column(idx)
     override def equals(row: Any) = row match {
-      case r:RowLike => this.row == r.content
-      case r:Seq[_] => this.row == r
+      case r: RowLike => this.row == r.content
+      case r: Seq[_] => this.row == r
       case _ => false
     }
-  }
-
+  }  
 }
 
 trait RowLike {
