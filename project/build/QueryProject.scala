0d18adb3a33898ebcbf5df08bbb5c0048b3b6788
Guntis Ozols <guntiso@uniso.lv>
2011-08-02 23:29:50 +0300

    scala 2.9.0-1 (with deprecations)

--- project/build/QueryProject.scala
+++ project/build/QueryProject.scala
@@ -8,13 +8,13 @@ class QueryProject(info: ProjectInfo) extends ParentProject(info)
 }
 
 protected class QueryCoreProject(info: ProjectInfo) extends DefaultProject(info) {
-      override def libraryDependencies = Set("org.scalatest" % "scalatest_2.8.1" % "1.5" % "test",
+      override def libraryDependencies = Set("org.scalatest" % "scalatest_2.9.0-1" % "1.6.1" % "test",
            "org.hsqldb" % "hsqldb-j5" % "2.2.4" % "test") ++ super.libraryDependencies 
 }
 
 protected
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  val liftVersion = "2.3"
+  val liftVersion = "2.4-M3"
 
   // uncomment the following if you want to use the snapshot repo
   //  val scalatoolsSnapshot = ScalaToolsSnapshots
@@ -23,13 +23,15 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   // this line
   // override def scanDirectories = Nil
 
+  lazy val JavaNet = "Java.net Maven2 Repository" at "http://download.java.net/maven/2/"
+
   override def libraryDependencies = Set(
     "net.liftweb" %% "lift-webkit" % liftVersion % "compile",
     "net.liftweb" %% "lift-mapper" % liftVersion % "compile",
-    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test",
-    "junit" % "junit" % "4.5" % "test",
+    "org.mortbay.jetty" % "jetty" % "6.1.26" % "test",
+    "junit" % "junit" % "4.7" % "test",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
-    "org.scala-tools.testing" %% "specs" % "1.6.6" % "test",
-    "com.h2database" % "h2" % "1.2.138"
+    "org.scala-tools.testing" %% "specs" % "1.6.8" % "test",
+    "com.h2database" % "h2" % "1.2.147"
   ) ++ super.libraryDependencies
 }
