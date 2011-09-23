898c6c145bd0185f73c4c21eb700b41617ad8dba
Janqis <jbirgelis@e-apollo.lv>
2011-09-23 15:56:40 +0300

    JAVA_OPTS is read from setenv file (in windows environment from setenv.bat)

--- sbt.bat
+++ sbt.bat
@@ -1,2 +1,6 @@
 set SCRIPT_DIR=%~dp0
-java %java_opts% -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx512M -Xss2M -jar "%SCRIPT_DIR%\sbt-launcher.jar" %*
+
+set JAVA_OPTS=
+if exist "%SCRIPT_DIR%\setenv.bat" call "%SCRIPT_DIR%\setenv.bat" %1
+
+java %JAVA_OPTS% -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256m -Xmx512M -Xss2M -jar "%SCRIPT_DIR%\sbt-launcher.jar" %*
