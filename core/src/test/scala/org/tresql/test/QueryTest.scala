cb5dbc6ae5f90295c13c11da2e7fba5ac36074ee
Martins Rumkovskis <martins@uniso.lv>
2012-02-27 13:20:22 +0200

    API test added

--- core/src/test/scala/org/tresql/test/QueryTest.scala
+++ core/src/test/scala/org/tresql/test/QueryTest.scala
@@ -93,6 +93,7 @@ class QueryTest extends Suite {
     expect(BigDecimal(10))(Query.unique[BigDecimal]("dept[10]{deptno}#(deptno)"))
     expect(5)(Query.unique[Int]("inc_val_5(?)", 0))
     expect(20)(Query.unique[Int]("inc_val_5(inc_val_5(?))", 10))
+    expect(15)(Query.unique[Long]("inc_val_5(inc_val_5(?))", 5))
     var op = OutPar()
     expect(List(10, "x"))(Query("in_out(?, ?, ?)", InOutPar(5), op, "x"))
     expect("x")(op.value)
