14e36d7bd1584bd987227de0ed043d683dfa743f
Martins Rumkovskis <martins@uniso.lv>
2013-03-02 13:11:54 +0200

    refactoring

--- core/src/test/scala/org/tresql/test/QueryTest.scala
+++ core/src/test/scala/org/tresql/test/QueryTest.scala
@@ -152,9 +152,12 @@ class QueryTest extends Suite {
       Query.first("dept[10]{deptno, dname, |emp[deptno = :1(1)]{ename} emps}") 
         { r => (r.dname, r.r.emps.map(r => r.ename).mkString(",")) }.get)
     //bind variables test
-    val ex = Query.build("dept[?]{deptno}")
-    expectResult(List(10, 20, 30, 40))(List(10, 20, 30, 40) flatMap {ex.select(_).map(_.deptno)})
-    ex.close
+    expectResult(List(10, 20, 30, 40)){
+      val ex = Query.build("dept[?]{deptno}")
+      val res = List(10, 20, 30, 40) flatMap {ex.select(_).map(_.deptno)}
+      ex.close
+      res
+    }
     //hierarchical inserts, updates test
     expectResult(List(1, List(List(1, 1))))(Query(
       """dept{deptno, dname, loc, +emp {empno, ename, deptno}[:empno, :ename, :deptno] emps} +
