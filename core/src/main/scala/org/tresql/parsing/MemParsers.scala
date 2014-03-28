bcc2bc9856329728fb1c97475f75d228af1ff732
Martins Rumkovskis <martins@uniso.lv>
2014-03-28 12:38:03 +0200

    refactoring

--- core/src/main/scala/org/tresql/parsing/MemParsers.scala
+++ core/src/main/scala/org/tresql/parsing/MemParsers.scala
@@ -2,7 +2,7 @@ package org.tresql.parsing
 
 trait MemParsers extends scala.util.parsing.combinator.Parsers {
   protected val intermediateResults = new ThreadLocal[scala.collection.mutable.Map[(String, Int), ParseResult[Any]]] {
-    override def initialValue = scala.collection.mutable.Map()
+    override def initialValue = scala.collection.mutable.HashMap()
   }
 
   class MemParser[+T](underlying: Parser[T]) extends Parser[T] {
