be21c3d8af74662108a728aed16ee7efb0028069
Martins Rumkovskis <martins@uniso.lv>
2014-08-26 01:29:15 +0300

    refactoring

--- src/main/scala/org/tresql/Macros.scala
+++ src/main/scala/org/tresql/Macros.scala
@@ -4,9 +4,9 @@ class Macros {
   def sql(b: QueryBuilder, const: QueryBuilder#ConstExpr) = b.SQLExpr(String valueOf const.value, Nil)
   
   def ~~ (b: QueryBuilder, lop: Expr, rop: Expr) =
-    b.BinExpr("~", b.FunExpr("upper", List(lop)), b.FunExpr("upper", List(rop)))
+    b.BinExpr("~", b.FunExpr("lower", List(lop)), b.FunExpr("lower", List(rop)))
 
   def !~~ (b: QueryBuilder, lop: Expr, rop: Expr) =
-    b.BinExpr("!~", b.FunExpr("upper", List(lop)), b.FunExpr("upper", List(rop)))
+    b.BinExpr("!~", b.FunExpr("lower", List(lop)), b.FunExpr("lower", List(rop)))
  
 }
