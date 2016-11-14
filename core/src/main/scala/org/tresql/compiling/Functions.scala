d627ff8371046cdfd6a2f6a5216cbce2733140ee
Martins Rumkovskis <martins@uniso.lv>
2016-11-15 01:11:58 +0200

    compiler - function refactoring

--- core/src/main/scala/org/tresql/compiling/Functions.scala
+++ core/src/main/scala/org/tresql/compiling/Functions.scala
@@ -9,14 +9,7 @@ trait DBAggregateFunctions {
   def avg[T](col: T): T
 }
 
-trait Functions extends DBAggregateFunctions {
-  def coalesce[T](pars: T*): T
-  def upper(string: String): String
-  def lower(string: String): String
-  def insert (str1: String, offset: Int, length: Int, str2: String): String
-  def to_date(date: String, format: String): java.sql.Date
-  def trim(string: String): String
-  def inc_val_5(int: java.lang.Integer): java.lang.Integer
+trait TresqlMacroFunctions {
   //macros
   def if_defined[T](variable: Any, exp: T): T
   def if_missing[T](variable: Any, exp: T): T
@@ -26,21 +19,47 @@ trait Functions extends DBAggregateFunctions {
   def if_all_missing(exp: Any*): Any
   def sql_concat(exprs: Any*): Any
   def sql(expr: Any): Any
-  //test macros function
-  def null_macros: Null
-  //dialect
-  def `case`[T](when: Any, `then`: T, rest: Any*): T
-  def nextval(seq: String): Any
+}
+
+trait TresqlScalaFunctions {
   //tresql scala functions
   import org.tresql.QueryCompiler._
   def mkString(sel: SelectDefBase): String
   def mkString(sel: SelectDefBase, colSep: String): String
   def mkString(sel: SelectDefBase, colSep: String, rowSep: String)
   def concat(string: String*): String
+}
+
+trait BasicDBFunctions {
+  def coalesce[T](pars: T*): T
+  def upper(string: String): String
+  def lower(string: String): String
+  def insert (str1: String, offset: Int, length: Int, str2: String): String
+  def to_date(date: String, format: String): java.sql.Date
+  def trim(string: String): String
+}
+
+trait BasicDialectFunctions {
+  //dialect
+  def `case`[T](when: Any, `then`: T, rest: Any*): T
+  def nextval(seq: String): Any
+}
+
+trait TresqlFunctions
+  extends DBAggregateFunctions
+  with TresqlMacroFunctions
+  with TresqlScalaFunctions
+  with BasicDBFunctions
+  with BasicDialectFunctions
+
+trait Functions extends TresqlFunctions {
+  //test macros function
+  def null_macros: Null
   //test tresql scala functions (must be registered here to be accessible during compile time)
   def echo(x: String): String
   def plus(a: java.lang.Long, b: java.lang.Long): java.lang.Long
   def average(a: BigDecimal, b: BigDecimal): BigDecimal
   def dept_desc(d: String, ec: String): String
   def nopars(): String
+  def inc_val_5(int: java.lang.Integer): java.lang.Integer
 }
