910f9524df7f64db9f54515dc0dc8f84a1afe5fd
Martins Rumkovskis <martins@uniso.lv>
2026-05-09 02:59:21 +0300

    compiler constructor

--- core/src/main/scala/org/tresql/QueryCompiler.scala
+++ core/src/main/scala/org/tresql/QueryCompiler.scala
@@ -2,7 +2,12 @@ package org.tresql
 
 class QueryCompiler(override val metadata: Metadata,
                     override val extraMetadata: Map[String, Metadata],
-                    macros: MacroResources)
+                    macros: MacroResources, cache: Cache)
   extends QueryParser(macros, null) with compiling.Compiler {
+    def this(
+      metadata: Metadata,
+      extraMetadata: Map[String, Metadata],
+      macros: MacroResources,
+    ) = this(metadata, extraMetadata, macros, null)
   def compile(exp: String): ast.Exp = compile(parseExp(exp))
 }
